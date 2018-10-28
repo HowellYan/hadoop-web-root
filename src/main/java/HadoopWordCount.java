0dc98a254c74ecca8e4ff36a32ac4b7bc09797e6
HowellYang <th15817161961@gmail.com>
2018-10-28 21:58:42 +0800

    wordcount

--- src/main/java/HadoopWordCount.java
+++ src/main/java/HadoopWordCount.java
@@ -1,6 +1,3 @@
-import java.io.IOException;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -11,39 +8,10 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class HadoopWordCount {
-
-    public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable>{
-
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
-
-        public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
-            }
-        }
-    }
-
-    public static class IntSumReducer
-            extends Reducer<Text,IntWritable,Text,IntWritable> {
-        private IntWritable result = new IntWritable();
-
-        public void reduce(Text key, Iterable<IntWritable> values,
-                           Context context
-        ) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
-            }
-            result.set(sum);
-            context.write(key, result);
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
@@ -55,8 +23,35 @@ public class HadoopWordCount {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
-        FileInputFormat.addInputPath(job, new Path("hdfs://127.0.0.1:9000"+"/profile"));
-        FileOutputFormat.setOutputPath(job, new Path("hdfs://127.0.0.1:9000"+"/out1"));
+        FileInputFormat.addInputPath(job, new Path("hdfs://127.0.0.1:9000" + "/profile"));
+        FileOutputFormat.setOutputPath(job, new Path("hdfs://127.0.0.1:9000" + "/out1"));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
     }
 }
